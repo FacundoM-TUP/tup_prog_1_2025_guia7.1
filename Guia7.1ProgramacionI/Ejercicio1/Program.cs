3c33394baa6af03b0cefb9a9d319f33b8244aec4
FacundoM-TUP <martinezfacundo942@gmail.com>
2025-05-29 18:05:12 -0300

    falta Ejercicio 3

---
+++ Guia7.1ProgramacionI/Ejercicio1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Defino variables
+            double total, mn1, mn2, mn3, mn4, porcion;
+
+            int en1, en2, en3, en4, sumed;
+            #endregion
+
+            #region Ingreso Datos
+            Console.WriteLine("Ingrese total a distribuir");
+
+            total = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("ingrese edades de las niñas");
+
+            en1 = Convert.ToInt32(Console.ReadLine());
+            en2 = Convert.ToInt32(Console.ReadLine());
+            en3 = Convert.ToInt32(Console.ReadLine());
+            en4 = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+            #region Realizo Operaciones
+            sumed = en1 + en2 + en3 + en4;
+
+            porcion = total / sumed;
+
+            mn1 = en1 * porcion;
+            mn2 = en2 * porcion;
+            mn3 = en3 * porcion;
+            mn4 = en4 * porcion;
+            #endregion
+
+            #region Muestro Resultados
+            Console.WriteLine("montos a distribuir:");
+            Console.WriteLine("para la primera nieta: {0:f2}", mn1);
+            Console.WriteLine("para la segunda nieta: {0:f2}", mn2);
+            Console.WriteLine("para la tercera nieta: {0:f2}", mn3);
+            Console.WriteLine("para la cuarta nieta: {0:f2}", mn4);
+
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
---
+++ Guia7.1ProgramacionI/Ejercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mpk, cms, cbd, cpq, ccj, sbd;
+
+            Console.WriteLine("ingrese cantidad de masa");
+            mpk = Convert.ToDouble(Console.ReadLine());
+
+            mpk = mpk * 1000;
+            cbd = mpk / 55;
+            cms = mpk % 55;
+            cpq = cbd / 12;
+            ccj = cpq / 20;
+            sbd = cbd - ccj * 20 * 12;
+
+            Console.WriteLine("Cantidad de budines {0:f2}", cbd);
+            Console.WriteLine("Masa Sobrante {0:f2}", cms);
+            Console.WriteLine("Paquetes a hacer {0:f2}", cpq);
+            Console.WriteLine("Cajas a completar {0:f2}", ccj);
+            Console.WriteLine("Budines sobrantes ", sbd);
+
+            Console.ReadKey();
+        }
+    }
+}
